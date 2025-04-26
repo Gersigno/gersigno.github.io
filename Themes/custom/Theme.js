8afdd6db3e318da9fa1ca604d9906d6801acb318
Gersigno <gersand.demaline@gmail.com>
2025-04-26 20:53:42 +0200

    Custom theme update

--- Themes/custom/Theme.js
+++ Themes/custom/Theme.js
@@ -4,5 +4,19 @@ import ThemeWallpaper from './Wallpaper/Wallpaper.js';
 export default class Theme extends ThemeBase {
     _index() {
         this.wallpaper = new ThemeWallpaper();
+
+        const taskbar = document.querySelector('#taskbar');
+        const taskbar_borders = document.createElement('div');
+        taskbar_borders.id = 'taskbar_borders';
+
+        taskbar.appendChild(taskbar_borders);
+    }
+
+    _destruct() {
+        const taskbar_borders = document.querySelector('#taskbar_borders');
+
+        if (taskbar_borders) {
+            taskbar_borders.remove();
+        }
     }
 }
