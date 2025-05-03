8400d164ba3b3e8659865cfe4b31705977524b94
Gersigno <gersand.demaline@gmail.com>
2025-05-03 20:26:14 +0200

    Custom theme update

--- Themes/custom/Theme.js
+++ Themes/custom/Theme.js
@@ -1,6 +1,7 @@
 import ThemeBase from '/System/Modules/ThemeBase.js';
 import ThemeWallpaper from './Wallpaper/Wallpaper.js';
 import StartMenu from '/System/Interface/HTML/StartMenu.js';
+import Highlight from './effects.js';
 
 export default class Theme extends ThemeBase {
     _index() {
@@ -9,6 +10,7 @@ export default class Theme extends ThemeBase {
         const taskbar = document.querySelector('#taskbar');
         const taskbar_borders = document.createElement('div');
         taskbar_borders.id = 'taskbar_borders';
+        taskbar_borders.classList.add('highlight_borders');
 
         taskbar.appendChild(taskbar_borders);
 
@@ -26,7 +28,17 @@ export default class Theme extends ThemeBase {
     _hookStartMenu() {
         const super_button = document.querySelector('#super');
         StartMenu.onOpened = () => {
+            const border_div = document.createElement('div');
+            const start_menu = document.querySelector('start-menu');
+
             super_button.classList.add('opened');
+
+            border_div.id = 'start_borders';
+            border_div.classList.add('highlight_borders');
+
+            start_menu.appendChild(border_div);
+
+            // Highlight.highlightElement(start_menu, 500);
         };
         StartMenu.onClosed = () => {
             super_button.classList.remove('opened');
